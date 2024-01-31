9159dc457c2959f2899036576e908d87acc7603d
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2024-01-30 22:19:54 -0600

    1/30 - Complete Math and Greet Portions

--- app.js
+++ app.js
@@ -15,11 +15,22 @@ app.get("/", (req, res) => {
 
 })
 
-// http://localhost:3000/greet?name=kaylee&dob=2002
+// http://localhost:3000/greet?name=priscilla&dob=2002
 app.get('/greet', (req, res)=> {
     console.log(req.query)
 
-    res.send(`hey, ${req.query.name}`)
+    // capitalizes name
+    let name = req.query.name
+
+    let capitalized =
+      name.charAt(0).toUpperCase()
+      + name.slice(1)
+
+    let currentYear = new Date().getFullYear()
+    let ageA = currentYear - req.query.dob - 1
+    let ageB = currentYear - req.query.dob
+
+    res.send(`Hello, ${capitalized}!\n You are ${ageA} or ${ageB} years old.`)
 })
 
 app.get('/math/:num1/:op/:num2', (req, res)=> {
@@ -29,7 +40,8 @@ app.get('/math/:num1/:op/:num2', (req, res)=> {
 
     if (operation == 'plus')
     {
-        result = req.params.num1 + req.params.num2
+        // when I use a plus sign, it combines the strings, so this is a substitute solution
+        result = req.params.num1 - (-req.params.num2)
     }
     
     else if (operation == 'minus') {
